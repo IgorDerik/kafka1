ed359a0a9dd458a08f1aaa653be4ca1f1bf2cb90
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-01-31 16:01:35 +0200

    preprod version

---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,22 @@
+package com.app;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "sandbox-hdp.hortonworks.com:6667");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        ProducerUtil.sendFromFile(props, args[0], args[1], Long.parseLong(args[2]), Integer.parseInt(args[3]));
+
+    }
+
+}
---
+++ src/main/java/com/app/ProducerUtil.java
@@ -0,0 +1,74 @@
+package com.app;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
+
+public class ProducerUtil {
+
+    public static void sendFromFile(Properties props, String topic, String filePath, long limitPointer, int parallelismDegree) {
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        long totalSize = 0; //debug var
+
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(filePath, "r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<String> stringList = new ArrayList<>();
+
+        String line = "";
+
+        long currLimitPointer = limitPointer;
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool(parallelismDegree);
+
+        try {
+            while (Objects.requireNonNull(raf).getFilePointer() < raf.length()) {
+
+                System.out.println("Current pointer: " + raf.getFilePointer()); //debug info
+
+                while ((raf.getFilePointer() < currLimitPointer) && ((line = raf.readLine()) != null)) {
+                    stringList.add(line);
+                }
+
+                System.out.println("Current collection size" + stringList.size()); //debug info
+                totalSize = totalSize + stringList.size();
+                System.out.println("TOTAL SIZE: " + totalSize); //debug info
+
+//            ForkJoinPool forkJoinPool = new ForkJoinPool(parallelismDegree);
+                Stream<String> stringStream = stringList.parallelStream();
+
+                stringStream.forEach(System.out::println); //debug info
+
+                forkJoinPool
+                        .submit(() -> stringStream.forEach(record -> producer.send(new ProducerRecord<>(topic, record))))
+                        .get();
+
+                stringList.clear();
+
+                currLimitPointer = currLimitPointer + limitPointer;
+            }
+        } catch (IOException|InterruptedException|ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Current total size" + totalSize); //debug info
+
+        producer.close();
+    }
+
+}
